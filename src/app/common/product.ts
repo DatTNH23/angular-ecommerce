fc9920468641c72c89868f9f83786bcb64e15785
Dat <tilamhanh123@gmail.com>
2023-09-10 21:24:59 +0700

    UpdateShoppingCartStatus

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,11 +1,11 @@
 export class Product {
     constructor(
-        public id: number,
-        public sku: String,
-        public name: String,
-        public description: String,
-        public unitPrice: String,
-        public imageUrl: String,
+        public id: string,
+        public sku: string,
+        public name: string,
+        public description: string,
+        public unitPrice: number,
+        public imageUrl: string,
         public active: boolean,
         public unitsInStock: number,
         public dateCreated: Date,
