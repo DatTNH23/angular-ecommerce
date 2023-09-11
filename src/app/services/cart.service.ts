ff868363558a9b87bee9f2d22dee67792daa0f4c
Dat <tilamhanh123@gmail.com>
2023-09-11 15:09:27 +0700

    Shopping Cart Items CRUD

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,6 +6,7 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class CartService {
+
   cartItems: CartItem[] = [];
   totalPrice: Subject<number> = new Subject<number>();
   totalQuantity: Subject<number> = new Subject<number>();
@@ -35,7 +36,7 @@ export class CartService {
 
     this.totalPrice.next(totalPriceValue)
     this.totalQuantity.next(totalQuantityValue)
-    this.logCartData(totalPriceValue, totalQuantityValue);
+    // this.logCartData(totalPriceValue, totalQuantityValue);
   }
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
 
@@ -47,5 +48,18 @@ export class CartService {
 
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('----');
+  }
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--
+    if (theCartItem.quantity === 0) this.remove(theCartItem)
+    else this.computeCartTotals()
+  }
+  remove(theCartItem: CartItem) {
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id)
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1)
+      this.computeCartTotals()
+    }
+
   }
 }
