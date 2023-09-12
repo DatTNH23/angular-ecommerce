9be93badd3c59ea5e1dacb7c4807e561142f764d
Dat <tilamhanh123@gmail.com>
2023-09-12 14:38:50 +0700

    Add Drop-down List in checkout forms

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
+import { ShopFormService } from 'src/app/services/shop-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -10,7 +13,16 @@ export class CheckoutComponent implements OnInit {
   checkoutFormGroup!: FormGroup;
   totalPrice: number = 0;
   totalQuantity: number = 0;
-  constructor(private formBuilder: FormBuilder) { }
+
+  creditCardYears: number[] = []
+  creditCardMonths: number[] = []
+
+  countries: Country[] = []
+  shippingAddressStates: State[] = []
+  billingAddressStates: State[] = []
+
+  constructor(private formBuilder: FormBuilder,
+    private shopForm: ShopFormService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -41,19 +53,77 @@ export class CheckoutComponent implements OnInit {
         expirationMonth: ['']
       }),
     })
+    const startMonth: number = new Date().getMonth() + 1
+    this.shopForm.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        this.creditCardMonths = data
+      }
+    )
+    this.shopForm.getCreditCardYears().subscribe(
+      data => {
+        this.creditCardYears = data
+      }
+    )
+    this.shopForm.getCountries().subscribe(
+      data => {
+        this.countries = data;
+      }
+    )
   }
-  onSubmit() {
-    console.log("Handling the submit button")
-  }
+
   copyShippingAddressToBillingAddress(event: any) {
 
     if (event.target.checked) {
       this.checkoutFormGroup.controls['billingAddress']
         .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+      this.billingAddressStates = this.shippingAddressStates
     }
     else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+      this.billingAddressStates = []
     }
 
+  }
+  onSubmit() {
+    console.log("Handling the submit button")
+  }
+
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard')
+    const currentYear: number = new Date().getFullYear()
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear)
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1
+    } else startMonth = 1
+    this.shopForm.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        this.creditCardMonths = data
+      }
+    )
+  }
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName)
+    const countryCode = formGroup?.value.country.code
+    const countryName = formGroup?.value.country.name
+    console.log(`formGroup: ${formGroup}`)
+    console.log(`countryCode: ${countryCode}`)
+    console.log(`countryName: ${countryName}`)
+    this.shopForm.getStates(countryCode).subscribe(
+      data => {
+        //console.log(this.billingAddressStates)
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data
+        } else {
+          console.log("I will print sth")
+
+          this.billingAddressStates = data
+          console.log(this.billingAddressStates)
+        }
+        formGroup?.get('state')?.setValue(data[0])
+      }
+    )
   }
 }
