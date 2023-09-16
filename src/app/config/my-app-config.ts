87a27f99a053ad3b8cab2293639e81dccb6360ca
Dat <tilamhanh123@gmail.com>
2023-09-16 22:28:23 +0700

    Adding security Login-Logout with Okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oab9wax6iIg31jwV5d7',
+        issuer: 'https://dev-76878408.okta.com/oauth/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
